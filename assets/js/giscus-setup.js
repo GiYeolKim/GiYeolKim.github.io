ddbb1980853c2756f74c7b6f9f0cc554e070b08a
GiYeolKim <93780518+GiYeolKim@users.noreply.github.com>
2025-12-10 02:43:11 +0000

    Deploying to gh-pages from @ GiYeolKim/GiYeolKim.github.io@6b955146964874c2c6faf733692b9631b1971c85 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "GiYeolKim/GiYeolKim",
+    "data-repo": "GiYeolKim/GiYeolKim.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
